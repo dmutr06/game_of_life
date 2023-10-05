c8f0a1d206dad48a1fd6a3c946b2357478637dc9
dmutro <shaduraa.dmutroo@gmail.com>
2023-10-05 18:08:42 +0300

    basic functional

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,114 @@
-use std::{fmt::Display, ops::{Deref, DerefMut}, process::Command};
+use std::{
+    fmt::Display,
+    ops::{Deref, DerefMut},
+    process::Command,
+};
 
 pub const DEAD_CHAR: char = ' ';
-pub const ALIVE_CHAR: char = '*';
+pub const ALIVE_CHAR: char = 'a';
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub enum Cell {
     Dead,
     Alive,
 }
 
-pub struct Screen(Vec<Vec<Cell>>);
+#[derive(Clone, Debug)]
+pub struct Screen {
+    screen: Vec<Vec<Cell>>,
+    width: usize,
+    height: usize,
+}
 
 impl Screen {
     pub fn new<T: Into<usize>>(width: T, height: T) -> Screen {
-        Screen(vec![vec![Cell::Dead; width.into()]; height.into()])
+        let (width, height) = (width.into(), height.into());
+        Screen {
+            screen: vec![vec![Cell::Dead; width]; height],
+            width,
+            height,
+        }
+    }
+
+    fn neighbours(&self, x: usize, y: usize) -> u8 {
+        let mut counter = 0;
+        for i in 0..=2 {
+            let y = y as i32;
+            let cur_y = y + i - 1;
+            if cur_y < 0 {
+                continue;
+            }
+            if let Some(line) = self.get(cur_y as usize) {
+                for j in 0..=2 {
+                    let x = x as i32;
+                    let cur_x: i32 = x + j - 1;
+
+                    if cur_y == y && cur_x == x {
+                        continue;
+                    }
+                    if cur_x < 0 {
+                        continue;
+                    }
+                    if let Some(Cell::Alive) = line.get(cur_x as usize) {
+                        counter += 1;
+                    }
+                }
+            }
+        }
+
+        counter
+    }
+}
+
+impl Iterator for Screen {
+    type Item = ();
+    fn next(&mut self) -> Option<Self::Item> {
+        print!("{}", &self);
+        let prev_frame = self.clone();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let cur = self[y][x];
+                let nbors = prev_frame.neighbours(x, y);
+                match cur {
+                    Cell::Dead => {
+                        if nbors == 3 {
+                            self[y][x] = Cell::Alive;
+                        }
+                    }
+                    Cell::Alive => {
+                        if nbors < 2 || nbors > 3 {
+                            self[y][x] = Cell::Dead;
+                        }
+                    }
+                }
+            }
+        }
+
+        if prev_frame.to_string() == self.to_string() {
+            return None;
+        }
+
+        Some(())
     }
 }
 
 impl Deref for Screen {
     type Target = Vec<Vec<Cell>>;
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.screen
     }
 }
 
 impl DerefMut for Screen {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        &mut self.screen
     }
 }
 
 impl Display for Screen {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut screen = String::new();
-        for y in &self.0 {
+        for y in &self.screen {
             for x in y {
                 screen.push((*x).into());
             }
@@ -41,8 +116,6 @@ impl Display for Screen {
         write!(f, "{}", screen)
     }
 }
-
-
 
 impl Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -63,13 +136,12 @@ impl Into<char> for Cell {
 #[cfg(windows)]
 pub fn clear_screen() {
     Command::new("cmd")
-            .args(["/c", "cls"])
-            .spawn()
-            .expect("cls command failed to start")
-            .wait()
-            .expect("failed to wait");
+        .args(["/c", "cls"])
+        .spawn()
+        .expect("cls command failed to start")
+        .wait()
+        .expect("failed to wait");
     if cfg!(target_os = "windows") {
-        
     } else {
         Command::new("clear")
             .spawn()
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::time::Duration;
 
-use game_of_life::{Screen, Cell, clear_screen};
+use game_of_life::{Cell, Screen};
 
 fn main() {
     let (width, height) = crossterm::terminal::size().unwrap();
 
     let mut screen = Screen::new(width, height);
-    
-    let mut i: usize = 1;
-    loop {
-        clear_screen();
-        print!("{}", screen);
-        screen[5][i] = Cell::Alive;
-        i += 1;
-        if i >= width.into() { break; }
+
+    screen[5][5] = Cell::Alive;
+    screen[5][4] = Cell::Alive;
+    screen[4][3] = Cell::Alive;
+    screen[4][5] = Cell::Alive;
+    screen[3][5] = Cell::Alive;
+
+    for _ in screen {
         std::thread::sleep(Duration::from_millis(200));
     }
 }
